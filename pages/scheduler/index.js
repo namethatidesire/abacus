fd4048e43c50aa1a4b688da6757208686dfb1091
AlMaini <abdullahalmaini2017@gmail.com>
2025-03-11 00:30:10 -0400

    #10: Initialized interface for fetching courses and setup temp page for demo

---
+++ pages/scheduler/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Scheduler from '../../components/scheduler';
+
+const SchedulerPage = () => {
+  return (
+    <div>
+        <Scheduler/>
+    </div>
+  );
+};
+
+export default SchedulerPage;
